34c4de45e95477285bb540a188f397782e551f81
Tito Villalobos <tito.villalobos.1@gmail.com>
2023-05-10 11:57:33 -0400

    Added vim-polyglot and updated zsh autocomplete location

--- vim.sh
+++ vim.sh
@@ -12,6 +12,7 @@ vim_plugin "ack_vim" "https://github.com/mileszs/ack.vim.git"
 vim_plugin "ctrlp" "https://github.com/ctrlpvim/ctrlp.vim.git"
 vim_plugin "vim-airline" "https://github.com/vim-airline/vim-airline.git"
 vim_plugin "tcomment" "https://github.com/tomtom/tcomment_vim"
+vim_plugin "vim-polyglot" "https://github.com/sheerun/vim-polyglot"
 
 # dir for macvim (specified in .vimrc)
 mkdir -p ~/.vim-swap
